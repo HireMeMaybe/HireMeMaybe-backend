f0d2f45f95e8a61ae356cdae6e8334594cc04c87
Pasu Sangiemsin <opxz7148@gmail.com>
2025-09-09 20:59:51 +0700

    fix(job post): Omit company detail from serialize job post and fix tags type

--- internal/model/job-post.go
+++ internal/model/job-post.go
@@ -4,13 +4,14 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 // Company is gorm model for store job post data in DB
 type JobPost struct {
 	ID        uint      `gorm:"primaryKey;autoIncrement;->" json:"id"`
 	CompanyID uuid.UUID `gorm:"not null;index;<-:create" json:"company_id"`
-	Company   Company   `gorm:"foreignKey:CompanyID;references:UserID" json:"company"`
+	Company   Company   `gorm:"foreignKey:CompanyID;references:UserID" json:"-"`
 
 	Title    string     `gorm:"type:text" json:"title"`
 	Desc     string     `gorm:"type:text" json:"desc"`
@@ -18,7 +19,7 @@ type JobPost struct {
 	Location string     `gorm:"type:text" json:"location"`
 	Type     string     `gorm:"type:text" json:"type"`
 	Salary   string     `gorm:"type:text" json:"salary"`
-	Tags     []string   `gorm:"type:text[]" json:"tags"`
+	Tags     pq.StringArray   `gorm:"type:text[]" json:"tags"`
 	PostTime time.Time  `gorm:"type:timestamp;default:CURRENT_TIMESTAMP;->" json:"post_time"`
 	Expiring *time.Time `gorm:"type:timestamp" json:"expiring,omitempty"`
 }
