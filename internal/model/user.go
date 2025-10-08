aab84c3e9303874ba1f6da72a4b8417a2cbc3b9c
0CreepySmile0 <yutivich2005@gmail.com>
2025-10-08 10:56:01 +0700

    feat(user-punishment): Added new `Punishment` field to `User` model

--- internal/model/user.go
+++ internal/model/user.go
@@ -2,6 +2,8 @@
 package model
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/lib/pq"
 	"gorm.io/gorm"
@@ -39,17 +41,24 @@ type EditableCompanyInfo struct {
 	Size     *string `json:"size" gorm:"check:size IN ('XS', 'S', 'M', 'L', 'XL')"`
 }
 
+type PunishmentStruct struct {
+	PunishmentType string    `json:"type"`
+	PunishAt       time.Time `json:"at"`
+	PunishEnd      time.Time `json:"end"`
+}
+
 // User struct is gorm model for store base user data in DB
 type User struct {
 	gorm.Model
-	Tel            *string   `json:"tel"`
-	Email          *string   `json:"email" gorm:"<-:create"`
-	ID             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey;<-:create" json:"id" `
-	GoogleID       string    `json:"-" gorm:"<-:create"`
-	Username       string    `json:"username" gorm:"<-:create"`
-	Password       string    `json:"-"`
-	Role           string    `json:"-"`
-	ProfilePicture string    `json:"profile_picture"`
+	Tel            *string          `json:"tel"`
+	Email          *string          `json:"email" gorm:"<-:create"`
+	ID             uuid.UUID        `gorm:"type:uuid;default:uuid_generate_v4();primaryKey;<-:create" json:"id" `
+	GoogleID       string           `json:"-" gorm:"<-:create"`
+	Username       string           `json:"username" gorm:"<-:create"`
+	Password       string           `json:"-"`
+	Role           string           `json:"-"`
+	Punishment     PunishmentStruct `json:"punishment"`
+	ProfilePicture string           `json:"profile_picture"`
 }
 
 // CPSKUser is gorm model for store CPSK student profile data in DB
